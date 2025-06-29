57fb146ea9e0cccc69694326e889941d0f352eb8
Daniel Paschoalinoto <danielpaschoalinoto@gmail.com>
2025-06-29 05:41:34 -0300

    Ajuste gh img

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const clickSound = document.getElementById('click-sound');
 
 downloadBtn.addEventListener('click', (event) => {
     console.log('Button clicked!');
-    clickSound.currentTime = 0; // Rewind to the start
+    clickSound.currentTime = 0;
     clickSound.play().catch(e => console.error("Error playing sound:", e));
 });
+
+document.addEventListener('contextmenu', event => event.preventDefault());
