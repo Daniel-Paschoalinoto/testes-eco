e8e1bf5069de9c00acd073c48d41027d2e8ce50f
Daniel Paschoalinoto <danielpaschoalinoto@gmail.com>
2025-07-01 13:14:36 -0300

    Inclusão vídeo placeholder

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ const downloadBtn = document.querySelector('.js-download-btn');
 const clickSound = document.getElementById('click-sound');
 
 downloadBtn.addEventListener('click', (event) => {
-    console.log('Button clicked!');
     clickSound.currentTime = 0;
     clickSound.play().catch(e => console.error("Error playing sound:", e));
 });
 
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-// Modal functionality
 const openWarningModalBtn = document.getElementById('openWarningModal');
 const warningModal = document.getElementById('warningModal');
 const closeButton = document.querySelector('.close-button');
